4563933fab00179d0263c97dfdd8d72fb593bf43
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-31 13:03:22 -0700

    schema and seeds

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO movies(movie_name)
+VALUES ("Smile"),
+        ("Friday the 13th"),
+        ("Halloween"),
+        ("Dawn of the Dead"),
+        ("Jaws");
+
+INSERT INTO reviews(review)
+VALUES  ("Really creepy"),
+        ("Jason is just misunderstood"),
+        ("That dude just won't die"),
+        ("WOW! a lot of zombies"),
+        ("Were gonna need a bigger boat");
